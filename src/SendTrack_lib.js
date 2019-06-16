90826427026a151d71344d46cce611edd6e746a6
bernschrodr <vlad60870@gmail.com>
2019-06-16 21:12:29 +0300

    lib: add Create object of youtube arguments

--- src/SendTrack_lib.js
+++ src/SendTrack_lib.js
@@ -1,33 +1,82 @@
 var ServicesUrl = {
-    YoutubeMusic : "youtube.music",
-    Youtube : "youtube",
-    
+    YoutubeMusic: "youtube.music",
+    Youtube: "youtube",
+
 }
 
-function InputStringWorker(inputUrl) {
+/*function CreateRequest(serviceObj){
+
+    
+    switch (serviceObj.service){
+        case ServicesUrl.Youtube :
+        var argarr = CreateYoutubeArguments(serviceObj.cutUrl);
+
+
+            break;
+    }
+    
+
+
+}*/
+
+function CreateYoutubeMusicArguments(){
+
+}
+
+function CreateYoutubeArguments(serviceObj){
+
+    var urlSubstring = serviceObj.cutUrl;
+
+    function eraseYoutubeWatchArg(urlSubstring){
+        var index = urlSubstring.indexOf('watch?');
+        return urlSubstring.substring(index + 'watch?'.length);
+    }
+
+    var result = {};
+
+    var arrayArguments = eraseYoutubeWatchArg(urlSubstring).split('&');
+    for(var i = 0; i < arrayArguments.length; i++){
+        var index = arrayArguments[i].indexOf("=");
+        var argument = arrayArguments[i].substring(0, index > 1 ? index - 1 : index);
+        var value = arrayArguments[i].substring(index + 1, arrayArguments[i].length);
+        result[argument] = value;
+    }
+    
+    return result;
+
+}
+
+function eraseDomain(urlString){
+    var index = urlString.indexOf('/');
+    return urlString.substring(index+1);
+}
+
+function CheckService(inputUrl) {
     var index = -1;
     var serviceName = "";
 
-    for (Service in ServicesUrl) {
-        if (checkServiceUrl(inputUrl, ServicesUrl[Service]) !== false) {
-            index = checkServiceUrl(inputUrl, ServicesUrl[Service]);
-            serviceName = Service;
-            break;
+    for (var Service in ServicesUrl) {
+        var finded = checkServiceUrl(inputUrl, ServicesUrl[Service]);
+        if (finded !== -1) {
+            index = finded;
+            serviceName = ServicesUrl[Service];
         }
     }
 
+    var cutUrl = inputUrl.substring(index);
+    cutUrl = eraseDomain(cutUrl);
+
     return {
-        service : serviceName,
-        cutUrl : inputUrl.substring(index),
+        service: serviceName,
+        cutUrl: cutUrl,
     }
 
 }
 
-function checkServiceUrl(UrlString, ServiceString) {
-    var index;
-    if (index = UrlString.indexOf(ServiceString) !== -1)
-        return index + ServiceString.length();
-    else
-        return false;
-}
+function checkServiceUrl(urlString, serviceString) {
+    var index = urlString.indexOf(serviceString);
+    if (index !== -1)
+        index += serviceString.length;
 
+    return index;
+}
