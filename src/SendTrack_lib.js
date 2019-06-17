5f5f22ba9e740b6bb564e3e55eab8779639a33d4
bernschrodr <vlad60870@gmail.com>
2019-06-18 02:26:53 +0300

    Youtube Object can be created

--- src/SendTrack_lib.js
+++ src/SendTrack_lib.js
@@ -4,12 +4,56 @@ var ServicesUrl = {
 
 }
 
+
+function createObjectToCompare(artist, track, url) {
+    return {
+        artist: artist,
+        track: track,
+        url: url,
+    }
+}
+
+function processInputUrl() {
+    var form = document.getElementById("urlForm");
+    var urlForm = document.getElementById("inputUrlForm");
+    var submitBtn = document.getElementById("submitBtn");
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        console.log('link was sent');
+
+        var resObj = requestYoutubeObject(
+            createYoutubeArguments(
+                checkService(urlForm.value)
+            )
+        );
+
+        var result = document.getElementById("result");
+        if (result.firstChild === null)
+            result.appendChild(document.createTextNode(JSON.stringify(resObj)));
+        else
+            result.firstChild.nodeValue = JSON.stringify(resObj);
+
+        var artist = resObj.items[0].channelTitle;
+        var track = resObj.items[0].snippet.title;
+        var url = "https://www.youtube.com/watch?v=" + resObj.items[0].id;
+
+        var objectToCompare = createObjectToCompare(artist, track, url);
+        var index = objectToCompare.artist.indexOf(" - Topic");
+        if(index !== -1)
+            objectToCompare.artist = objectToCompare.artist.substring(0, index - 1);
+
+    });
+
+}
+
+
 /*function CreateRequest(serviceObj){
 
     
     switch (serviceObj.service){
         case ServicesUrl.Youtube :
-        var argarr = CreateYoutubeArguments(serviceObj.cutUrl);
+        var argarr = createYoutubeArguments(serviceObj.cutUrl);
 
 
             break;
@@ -22,32 +66,45 @@ var ServicesUrl = {
 function requestYoutubeObject(argumentsObj) {
 
     var xhr = new XMLHttpRequest();
-    var PREFIX = "";
-    for(var argument in argumentsObj){
-        PREFIX += argument + '='
+    var YOUTUBE_API_SERVER = "https://www.googleapis.com/youtube/v3/videos?part=snippet";
+    var API_KEY = "AIzaSyBEYdv5D-1VmQHgb5d3jR2qn2mo_mvlr9g";
+    var resultRequest = YOUTUBE_API_SERVER;
+
+    for (var argument in argumentsObj) {
+        switch (argument) {
+            case "v":
+                resultRequest += "&id=" + argumentsObj[argument];
+                break;
+            default:
+                break;
+        }
     }
 
+    resultRequest += "&key=" + API_KEY;
 
-    xhr.open('GET', 'phones.json', false);
+    xhr.open('GET', resultRequest, false);
 
     // 3. Отсылаем запрос
     xhr.send();
 
+    var videoInfoObj = JSON.parse(xhr.responseText);
+
     // 4. Если код ответа сервера не 200, то это ошибка
-    if (xhr.status != 200) {
+    if (xhr.status !== 200) {
         // обработать ошибку
         alert(xhr.status + ': ' + xhr.statusText); // пример вывода: 404: Not Found
     } else {
         // вывести результат
         alert(xhr.responseText); // responseText -- текст ответа.
     }
+    return videoInfoObj;
 }
 
-function CreateYoutubeMusicArguments() {
+function createYoutubeMusicArguments() {
 
 }
 
-function CreateYoutubeArguments(serviceObj) {
+function createYoutubeArguments(serviceObj) {
 
     var urlSubstring = serviceObj.cutUrl;
 
@@ -75,7 +132,7 @@ function eraseDomain(urlString) {
     return urlString.substring(index + 1);
 }
 
-function CheckService(inputUrl) {
+function checkService(inputUrl) {
     var index = -1;
     var serviceName = "";
 
