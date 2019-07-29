900c151a10d2f58466e85d9bb92467ec58d4f35d
Vladislav <vlad60870@gmail.com>
2019-07-30 00:32:34 +0300

    add /build

---
+++ build/precache-manifest.4f9ae93273e5bb3f0a0de19fae0c1472.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc9f8897a620d2db4e15e24b72304cc2",
+    "url": "/SendTrack/index.html"
+  },
+  {
+    "revision": "d7892773a6ac7474c511",
+    "url": "/SendTrack/static/css/main.21a27278.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/SendTrack/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "d7892773a6ac7474c511",
+    "url": "/SendTrack/static/js/main.9a919db8.chunk.js"
+  },
+  {
+    "revision": "af8abff73274d1980c43",
+    "url": "/SendTrack/static/js/runtime~main.c2ca175d.js"
+  }
+]);
